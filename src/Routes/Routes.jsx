b84fa099a8861ed1867f6c3e0c7905241fdedb27
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-07-04 03:40:45 +0600

    Create all Routes

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -1,8 +1,21 @@
 import { createBrowserRouter } from "react-router-dom";
+import MainLayout from "../Layout/MainLayout";
+import Home from "../Pages/Home/Home";
+import About from "../Pages/About/About";
+import Appointment from "../Pages/Appointment/Appointment";
+import Login from "../Pages/Login/Login";
+import SignUp from "../Pages/SignUp/SignUp";
 
 export const router = createBrowserRouter([
     {
       path: "/",
-      element: <div>Hello world!</div>,
+      element: <MainLayout/>,
+      children: [
+        { path: "/", element: <Home/> },
+        { path: "/about", element: <About/> },
+        { path: "/appointment", element: <Appointment/>},
+        { path: "/login", element: <Login/>},
+        {path:"/signup", element: <SignUp/>}
+      ],
     },
   ]);
