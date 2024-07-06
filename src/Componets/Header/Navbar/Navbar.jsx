5988714427aed1db9e826bbaa49b0297e1d82732
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-07-06 22:57:55 +0600

    create navbar open and close icon

--- src/Componets/Header/Navbar/Navbar.jsx
+++ src/Componets/Header/Navbar/Navbar.jsx
@@ -1,30 +1,44 @@
-import { Link, NavLink } from 'react-router-dom'
-import brandLogo from '../../../assets/logo/brandLogo.svg'
-import { menu } from '../../../Utils/menu'
+import { Link, NavLink } from "react-router-dom";
+import brandLogo from "../../../assets/logo/brandLogo.svg";
+import { menu } from "../../../Utils/menu";
+import { useState } from "react";
+import { IoClose, IoMenu } from "react-icons/io5";
 
 const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
 
   return (
-    <div className='flex justify-between items-center'>
-        <Link to='/' className='flex justify-center items-center gap-2 '>
-            <img src={brandLogo} alt="Doc Connect Logo" className=''/>
-            <p className='text-lg sm:text-xl md:text-2xl lg:text-3xl font-bold'>
-                <span className='text-light-coffee'>Doc</span> Connect
-            </p>
-        </Link>
-        <div>
-            <ul className='flex justify-center items-center gap-5'>
-                {menu?.map((item,index)=>(
-                    <li key={index}>
-                        <NavLink to={item.path} className='text-xl sm:text-2xl md:text-3xl hover:text-light-gray transition-colors duration-500'>
-                            {item.title}
-                        </NavLink>
-                    </li>
-                ))}
-            </ul>
-        </div>
+    <div className="flex justify-between items-center">
+      <Link to="/" className="flex justify-center items-center gap-2">
+        <img src={brandLogo} alt="Doc Connect Logo" className="" />
+        <p className="text-lg sm:text-xl md:text-2xl lg:text-3xl font-bold">
+          <span className="text-light-coffee">Doc</span> Connect
+        </p>
+      </Link>
+      <div>
+        <ul className="flex justify-center items-center gap-5">
+          {menu?.map((item, index) => (
+            <li key={index}>
+              <NavLink
+                to={item.path}
+                className="text-xl sm:text-2xl md:text-3xl hover:text-light-gray transition-colors duration-500"
+              >
+                {item.title}
+              </NavLink>
+            </li>
+          ))}
+        </ul>
+      </div>
+      <div>
+        <button>
+          <IoMenu className="text-xl sm:text-2xl md:text-3xl hover:text-light-gray transition-colors duration-500" />
+        </button>
+        <button>
+          <IoClose className="text-xl sm:text-2xl md:text-3xl hover:text-light-gray transition-colors duration-500" />
+        </button>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
