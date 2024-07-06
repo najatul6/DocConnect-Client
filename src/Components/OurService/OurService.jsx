d72da440079371a8c112808556fd2238dbf66e1c
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-07-07 04:58:50 +0600

    create our service section

---
+++ src/Components/OurService/OurService.jsx
@@ -0,0 +1,65 @@
+import { NavLink } from "react-router-dom";
+import ServiceCard from "../ServiceCard/ServiceCard";
+
+const OurService = () => {
+  return (
+    <div className="flex flex-col justify-center items-start gap-10">
+      <div className="flex flex-col justify-center items-start gap-5">
+      <h2 className="text-lg sm:text-xl md:text-2xl font-bold">Our Services</h2>
+      <p className="text-base">
+        Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+        accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab
+        illo inve ntore veritatis et quasi architecto beatae vitae dicta sunt
+        explicabo.
+      </p>
+      </div>
+      <div className=" border-[#828282] border rounded-xl">
+        <ul>
+          <li className="flex justify-center items-center">
+            <NavLink
+              className={({ isActive, isPending }) =>
+                isPending
+                  ? "pending"
+                  : isActive
+                  ? "shadow-[#f7a582_0px_0px_5px] text-black bg-light-coffee  px-5 py-5 rounded-xl text-xl font-semibold"
+                  : "hover:shadow-[#f7a582_0px_0px_5px] hover:text-light-coffee px-5 py-5 rounded-xl text-xl font-semibold"
+              }
+              to="/p"
+            >
+              Cavity Protection
+            </NavLink>
+            <NavLink
+              className={({ isActive, isPending }) =>
+                isPending
+                  ? "pending"
+                  : isActive
+                  ? "shadow-[#f7a582_0px_0px_5px] text-black bg-light-coffee  px-5 py-5 rounded-xl text-xl font-semibold"
+                  : "hover:shadow-[#f7a582_0px_0px_5px] hover:text-light-coffee px-5 py-5 rounded-xl text-xl font-semibold"
+              }
+              to="/service1"
+            >
+              Cosmetic Dentistry
+            </NavLink>
+            <NavLink
+              className={({ isActive, isPending }) =>
+                isPending
+                  ? "pending"
+                  : isActive
+                  ? "shadow-[#f7a582_0px_0px_5px] text-black bg-light-coffee  px-5 py-5 rounded-xl text-xl font-semibold"
+                  : "hover:shadow-[#f7a582_0px_0px_5px] hover:text-light-coffee px-5 py-5 rounded-xl text-xl font-semibold"
+              }
+              to="/service1"
+            >
+              Oral Surgery
+            </NavLink>
+          </li>
+        </ul>
+      </div>
+      <div>
+        <ServiceCard/>
+      </div>
+    </div>
+  );
+};
+
+export default OurService;
---
+++ src/Components/ServiceCard/ServiceCard.jsx
@@ -0,0 +1,24 @@
+import cardImg from '../../assets/banner/teeth.png'
+const ServiceCard = () => {
+  return (
+    <div className='flex flex-col justify-center items-center gap-5 '>
+        <div>
+        <img src={cardImg} alt="" />
+        </div>
+        <div className='space-y-5 border grid grid-cols-1 gap-10'>
+          <h4>Electro  Gastrology Therapy</h4>
+          <p>
+          Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inve ntore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Sed ut perspiciatis unde omnis iste natus error 
+          </p>
+          <p>
+          Sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inve ntore veritatis et quasi architecto beatae vitae dicta sunt explicabo.
+          </p>
+          <button>
+          More Details
+          </button>
+        </div>
+    </div>
+  )
+}
+
+export default ServiceCard
