606ce353155078fbfe91104f751541995728841f
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-07-10 13:39:14 +0600

    Create Doctor Card

--- src/Components/DoctorSection/DoctorCard.jsx
+++ src/Components/DoctorSection/DoctorCard.jsx
@@ -5,7 +5,7 @@ const DoctorCard = () => {
   <figure>
     <img
       src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg"
-      alt="Shoes" />
+      alt="Profile Images" />
   </figure>
   <div className="card-body">
     <h2 className="card-title">Shoes!</h2>
