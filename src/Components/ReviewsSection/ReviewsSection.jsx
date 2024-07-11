a00ed3b52e23ed08a42e3c2315d0b45e29635d78
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-07-11 23:08:48 +0600

    edit text for review card

--- src/Components/ReviewsSection/ReviewsSection.jsx
+++ src/Components/ReviewsSection/ReviewsSection.jsx
@@ -7,7 +7,7 @@ const ReviewsSection = () => {
         <h1 >What Our Patients Says</h1>
         <p>
           Sed ut perspiciatis unde omnis iste natus error sit voluptatem
-          accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae
+          accusantium dolor emque laudanum, totam rem aperiam, eaque ipsa quae
           ab illo inve ntore veritatis et quasi architecto beatae vitae dicta
           sunt explicabo.
         </p>
