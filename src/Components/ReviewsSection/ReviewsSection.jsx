d9fd99e949ad09fb47129d29f2405dc4271febbe
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-07-15 10:10:14 +0600

    fix review section and provide title ui design

--- src/Components/ReviewsSection/ReviewsSection.jsx
+++ src/Components/ReviewsSection/ReviewsSection.jsx
@@ -3,7 +3,7 @@ import ReviewCard from "./ReviewCard";
 const ReviewsSection = () => {
   return (
     <div className="mx-auto">
-      <div className="flex flex-col justify-center items-center text-center">
+      <div className="flex flex-col justify-center items-center text-center w-[75%] mx-auto my-5">
         <h1 className="text-3xl font-bold">What Our Patients Says</h1>
         <p className="text-lg">
           Sed ut perspiciatis unde omnis iste natus error sit voluptatem
