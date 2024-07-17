83e3dcb8ed1ebc67a92d4e32827fc5d5659f720f
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-07-17 21:56:10 +0600

    Create a basic form in contact form

--- src/Components/ContactWithUs/ContactForm.jsx
+++ src/Components/ContactWithUs/ContactForm.jsx
@@ -3,7 +3,12 @@ import { Form } from "react-router-dom"
 const ContactForm = () => {
   return (
     <Form>
-        
+        <input type="text" placeholder="Name" />
+        <input type="email" placeholder="Email" />
+        <input type="tel" placeholder="Mobile Number" />
+        <input type="text" placeholder="Doctor Name" />
+        <input type="date" placeholder="Date" />
+        <input type="time" placeholder="Time" />
     </Form>
   )
 }
