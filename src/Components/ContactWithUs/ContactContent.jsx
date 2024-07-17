403956e0e59e96d9959f5457c7f17b181b19fa49
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-07-17 22:09:13 +0600

    Complete Contact Content section

--- src/Components/ContactWithUs/ContactContent.jsx
+++ src/Components/ContactWithUs/ContactContent.jsx
@@ -2,19 +2,19 @@ import { FaLocationDot, FaPhoneVolume } from "react-icons/fa6";
 
 const ContactContent = () => {
   return (
-    <div>
-      <h2>Contact With Us</h2>
-      <p>
+    <div className="flex flex-col gap-5 justify-center items-start">
+      <h2 className="text-3xl font-bold capitalize">Contact With Us</h2>
+      <p className="text-base text-justify">
         Sed ut perspiciatis unde omnis iste natus error sit voluptatem
         accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab
         illo inve ntore veritatis et quasi.
       </p>
-      <div>
-        <p className="flex justify-start items-center">
+      <div className="flex flex-col gap-3">
+        <p className="flex justify-start items-center text-lg font-bold gap-5">
           <FaPhoneVolume />
           +88 01750 14 14 14
         </p>
-        <p className="flex justify-start items-center">
+        <p className="flex justify-start items-center text-lg font-bold gap-5">
           <FaLocationDot />
           Dhanmondi, Dhaka, Bangladesh
         </p>
