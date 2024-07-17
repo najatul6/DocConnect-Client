b441013b98f918ae09b3733d1b8a5ca457c70678
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-07-17 22:21:34 +0600

    Create a great from input in contact from

--- src/Components/ContactWithUs/ContactForm.jsx
+++ src/Components/ContactWithUs/ContactForm.jsx
@@ -4,14 +4,56 @@ const ContactForm = () => {
   return (
     <Form>
       <div className="grid grid-cols-2 gap-5">
-        <input type="text" placeholder="Name" className="p-5 rounded-xl bg-[#FFFFFF0D]" />
-        <input type="email" placeholder="Email"  className="p-5 rounded-xl bg-[#FFFFFF0D]"/>
-        <input type="tel" placeholder="Mobile Number"  className="p-5 rounded-xl bg-[#FFFFFF0D]"/>
-        <input type="text" placeholder="Doctor Name"  className="p-5 rounded-xl bg-[#FFFFFF0D]"/>
-        <input type="date" placeholder="Date"  className="p-5 rounded-xl bg-[#FFFFFF0D]"/>
-        <input type="time" placeholder="Time"  className="p-5 rounded-xl bg-[#FFFFFF0D]"/>
+        <input
+          type="text"
+          name="name"
+          required
+          placeholder="Name"
+          className="p-5 rounded-xl bg-[#FFFFFF0D]"
+        />
+        <input
+          type="email"
+          name="email"
+          required
+          placeholder="Email"
+          className="p-5 rounded-xl bg-[#FFFFFF0D]"
+        />
+        <input
+          type="tel"
+          pattern="[0-9]{3}[0-9]{3}[0-9]{4}"
+          name="mobileNumber"
+          required
+          placeholder="123-456-7890"
+          className="p-5 rounded-xl bg-[#FFFFFF0D]"
+        />
+        <input
+          type="text"
+          name="doctorName"
+          required
+          placeholder="Doctor Name"
+          className="p-5 rounded-xl bg-[#FFFFFF0D]"
+        />
+        <input
+          type="date"
+          name="date"
+          required
+          placeholder="Date"
+          className="p-5 rounded-xl bg-[#FFFFFF0D]"
+        />
+        <input
+          type="time"
+          name="time"
+          required
+          placeholder="Time"
+          className="p-5 rounded-xl bg-[#FFFFFF0D]"
+        />
       </div>
-      <button type="submit" className="p-5 text-white rounded-xl bg-light-coffee mt-6 w-full hover:bg-transparent font-bold duration-300 border-2 border-light-coffee">Book Now</button>
+      <button
+        type="submit"
+        className="p-5 text-white rounded-xl bg-light-coffee mt-6 w-full hover:bg-transparent font-bold duration-300 border-2 border-light-coffee"
+      >
+        Book Now
+      </button>
     </Form>
   );
 };
