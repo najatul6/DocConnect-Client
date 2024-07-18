63c602d61289c755a756dc0ecbe955920b4d2a58
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-07-18 19:49:56 +0600

    Edit home section

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -25,84 +25,25 @@ const Home = () => {
       </div>
 
       {/* Opening Details Section  */}
-      <div className="p-5">
+      <div className="p-5 my-5">
         <OpeningSection />
       </div>
 
       {/* Reviews Section  */}
-      <div>
+      <div className="p-5 my-5">
         <ReviewsSection />
       </div>
 
       {/* Doctor Section  */}
-      <div>
+      <div className="p-5 my-5">
         <DoctorSection />
       </div>
       {/* Contact With Us Section  */}
-      <div>
+      <div className="p-5 my-5">
         <ContactWithUs />
       </div>
 
-      <div>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam quia id
-        eos sapiente dolorem dicta et eaque iste nemo. Cumque nulla itaque
-        maxime ducimus aliquam amet temporibus enim expedita reprehenderit?
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Molestias
-        totam, modi quis minus eius, blanditiis obcaecati sint tempora aperiam
-        voluptatem esse quibusdam. Nobis dolore, exercitationem ipsa quas totam
-        quo eveniet enim quam non iure quasi aliquam minima necessitatibus alias
-        ratione ex! Tenetur, qui iure. Labore necessitatibus pariatur itaque
-        similique laudantium ea quo iusto molestiae quasi, qui beatae delectus
-        ipsum laboriosam sint est dolorum tenetur numquam praesentium commodi.
-        Facilis, incidunt accusamus accusantium, dolorum adipisci omnis nulla
-        quasi nihil maiores atque, laboriosam a consequuntur debitis. Dolor
-        quisquam tempora voluptatem. Laborum iste dolor officia. Nostrum
-        excepturi, modi maiores quibusdam omnis assumenda voluptates. A, alias
-        culpa? Perferendis, voluptatum eligendi veritatis, consequuntur at
-        consectetur magni est ipsam sequi ea eius temporibus quae nulla
-        doloribus unde impedit distinctio animi omnis obcaecati repellendus
-        laboriosam modi illo possimus eveniet. Quasi laboriosam, facere porro
-        dolore tempore reprehenderit ullam eveniet unde, itaque iure repellat
-        dignissimos veniam possimus molestias optio nostrum doloribus ex
-        tenetur, eius odit minima nobis. Similique laborum voluptatum, sit quas
-        aliquid dolore officiis reiciendis! Earum voluptatum quo vero placeat,
-        eveniet maxime, quasi inventore nihil nam esse aspernatur architecto ad
-        deserunt ducimus nemo temporibus voluptatem accusantium non quia
-        possimus atque. Cumque possimus est eum nesciunt, molestias, itaque iure
-        aperiam ex excepturi maxime soluta facere suscipit illum quibusdam iste
-        debitis quisquam in labore provident! Vitae beatae eveniet nostrum
-        mollitia ipsam ad doloremque ex, animi, eum iure autem. Cupiditate iste
-        temporibus mollitia ipsum tempora saepe? Voluptate aliquam dolor
-        voluptatibus eveniet architecto error tempora inventore aut iure
-        pariatur. Necessitatibus velit, autem dolore unde quod alias numquam quo
-        dolor quae explicabo quos asperiores magnam molestiae aliquam excepturi
-        fugit vitae quis illo officiis voluptatem omnis minima sed labore
-        inventore. Sint velit alias totam harum suscipit temporibus iste
-        corporis architecto non ea nesciunt, porro eaque in? Sed amet voluptas
-        eveniet minima. Facere beatae illum excepturi suscipit corrupti saepe
-        nulla delectus, labore ipsum, sapiente harum perspiciatis adipisci
-        corporis? Soluta reprehenderit quia placeat nobis quis, et voluptatibus
-        dignissimos saepe, at mollitia facere dolores aliquam. Magni debitis
-        iure necessitatibus, optio itaque, sed error tempora nam accusantium
-        provident, fuga odio neque? Cum alias facilis eligendi adipisci totam
-        sunt consectetur ab neque obcaecati necessitatibus aliquid nulla vero
-        beatae, fugit, blanditiis molestiae, nisi optio. Adipisci itaque ex
-        possimus ad quam unde repudiandae tempore magnam molestiae, enim sit
-        similique eligendi harum vel incidunt alias obcaecati totam delectus
-        modi. Quam perspiciatis quo ducimus! Laboriosam illum quia illo sit
-        consequatur iusto odio, saepe reiciendis maiores quas modi minus,
-        expedita obcaecati quasi nam. Ea voluptatem, itaque, delectus magni
-        cupiditate quia ipsum sapiente aliquid earum alias cumque impedit
-        voluptates praesentium quae ipsam? Labore ipsum quam distinctio quia
-        iusto recusandae itaque minus assumenda, quis soluta error nam, quaerat,
-        voluptatem quo sit exercitationem pariatur tempore accusamus
-        praesentium? Ea repellendus suscipit autem libero natus incidunt? Nulla
-        ab molestiae, officia id nobis voluptatem quia doloremque at? Nemo
-        consectetur facere at tempora earum expedita officiis quam, ad vitae
-        sunt autem natus, beatae quisquam architecto. Laudantium, repudiandae,
-        voluptate ipsam repellat quo ea, qui eius a possimus dolorum asperiores
-        provident odit temporibus labore!
-      </div>
+     
     </div>
   );
 };
