dde4dd2577d3e6b0b09413eef5e29ebcb51cd17b
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-07-24 22:43:48 +0600

    fixed color for footer paragraph

--- src/Components/Shared/Footer/Footer.jsx
+++ src/Components/Shared/Footer/Footer.jsx
@@ -5,6 +5,7 @@ import Button from "../Button/Button";
 const Footer = () => {
   return (
     <>
+    
     <div className="custom-footer py-28 px-40 flex justify-evenly items-center gap-10 shadow-xl shadow-dark-coffee">
       <div className="flex flex-col justify-start items-start gap-5">
         <Link to="/" className="flex justify-center items-center gap-2">
@@ -59,7 +60,7 @@ const Footer = () => {
         </ul>
       </div>
     </div>
-    <p className="text-center text-lg font-medium text-[#828282] w-full py-5">&copy; Alright Reserved by - <Link to="https://www.github.com/najatul6" className="font-bold text-blue-400 hover:text-blue-600">Najatul islam</Link></p>
+    <p className="text-center text-lg font-medium text-white w-full py-5 bg-light-coffee">&copy; Alright Reserved by - <Link to="https://www.github.com/najatul6" className="font-bold text-light-green hover:text-dark-coffee">Najatul islam</Link></p>
     </>
   );
 };
