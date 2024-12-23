582c095f7af3792e54c7bb0918f2451b802a92c9
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-12-23 22:16:54 +0600

    update doctors section

--- src/Components/DoctorSection/DoctorSection.jsx
+++ src/Components/DoctorSection/DoctorSection.jsx
@@ -11,7 +11,7 @@ const DoctorSection = () => {
           "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inve ntore veritatis et quasi architecto beatae vitae dicta sunt explicabo."
         }
       />
-      <div className="grid grid-cols-3 gap-5 my-10">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 my-10">
         {
           doctorsInfo.map(doctor=>{
             return <DoctorCard key={doctor.id} doctor={doctor} />
