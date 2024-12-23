7b8067f394fcce9d8f703b461a65da7d9c29bd1d
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-12-23 22:27:20 +0600

    update navbar background

--- src/Components/Header/Navbar/Navbar.jsx
+++ src/Components/Header/Navbar/Navbar.jsx
@@ -8,7 +8,7 @@ const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
 
   return (
-    <header className="fixed right-0 left-0 max-w-[1920px] mx-auto py-[1.3rem] px-[2%] lg:py-5 lg:px-[2%] flex justify-between items-center z-[100] before:content-[''] before:top-0 before:left-0 before:absolute before:w-full before:h-full before:backdrop-blur-[50px] before:z-[-1] text-white font-bold shadow-xl lg:shadow-none">
+    <header className="fixed right-0 left-0 max-w-[1920px] mx-auto py-[1.3rem] px-[2%] lg:py-5 lg:px-[2%] flex justify-between items-center z-[100] before:content-[''] before:top-0 before:left-0 before:absolute before:w-full before:h-full before:bg-black before:z-[-1] text-white font-bold shadow-xl lg:shadow-none">
       <div className="text-3xl font-bold lg:leading-[70px] capitalize cursor-pointer">
         <Link to="/" className="flex justify-center items-center gap-2">
           <img src={brandLogo} alt="Doc Connect Logo" className="w-[15%]" />
@@ -31,7 +31,7 @@ const Navbar = () => {
       <nav
         className={`${
           isOpen ? "h-[17 rem]" : "h-0 overflow-hidden"
-        } top-full  absolute left-0 w-full bg-gradient-back backdrop-blur-2xl shadow-2xl shadow-gradient-back transition-all duration-500 lg:h-auto lg:static lg:w-auto lg:bg-inherit lg:backdrop-blur-0 lg:shadow-none delay-700 lg:delay-0`}
+        } top-full  absolute left-0 w-full bg-gradient-back lg:bg-gradient-back  backdrop-blur-2xl shadow-2xl shadow-gradient-back transition-all duration-500 lg:h-auto lg:static lg:w-auto lg:bg-inherit lg:backdrop-blur-0 lg:shadow-none delay-700 lg:delay-0`}
       >
         <ul className="flex flex-col lg:flex-row justify-between items-center gap-6 px-5 py-5 md:py-2 lg:py-0">
           {menu?.map((route, index) => (
